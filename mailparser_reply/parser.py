673ceea3055b03b0c105f3268379a4d34750e714
alfons <alfonsrv@protonmail.com>
2023-01-04 23:22:06 +0100

    Initial commit 🚀

--- mailparser_reply/parser.py
+++ mailparser_reply/parser.py
@@ -1,17 +1,4 @@
-#  Copyright (c) 2022 Rau Systemberatung GmbH. All rights reserved.
-#
-#  Licensed under the GNU General Public License, Version 3 (the "License").
-#  You may not use this file except in compliance with the License.
-#  A copy of the License is located at:
-#    https://www.gnu.org/licenses/gpl-3.0.html
-#  or in the "license" file accompanying this file. This file is distributed
-#  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-#  express or implied. See the License for the specific language governing
-#  permissions and limitations under the License.
-#
-#  Selling exceptions may apply as per written contractual agreements between
-#  commercial entities and Rau Systemberatung GmbH, releasing the former from
-#  the License obligations.
+# Copyright (c) 2023 – RAUSYS, Rau Systemberatung GmbH
 
 import logging
 import re
